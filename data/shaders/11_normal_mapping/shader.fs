5e056ef7c3ccb9f09023637aa4c005bd5ebaa11b
ultimatenick <nega9705@gmail.com>
2021-12-13 22:30:14 +0100

    modified exercise 11

--- data/shaders/11_normal_mapping/shader.fs
+++ data/shaders/11_normal_mapping/shader.fs
@@ -4,9 +4,8 @@ out vec4 FragColor;
 in VS_OUT {
     vec3 FragPos;
     vec2 TexCoords;
-    vec3 TangentLightPos;
-    vec3 TangentViewPos;
-    vec3 TangentFragPos;
+    vec3 LightDir;
+    vec3 ViewDir;
 } fs_in;
 
 uniform sampler2D diffuseMap;
@@ -27,13 +26,11 @@ void main()
     // ambient
     vec3 ambient = 0.1 * color;
     // diffuse
-    vec3 lightDir = normalize(fs_in.TangentLightPos - fs_in.TangentFragPos);
-    float diff = max(dot(lightDir, normal), 0.0);
+    float diff = max(dot(fs_in.LightDir, normal), 0.0);
     vec3 diffuse = diff * color;
     // specular
-    vec3 viewDir = normalize(fs_in.TangentViewPos - fs_in.TangentFragPos);
-    vec3 reflectDir = reflect(-lightDir, normal);
-    vec3 halfwayDir = normalize(lightDir + viewDir);  
+    vec3 reflectDir = reflect(-fs_in.LightDir, normal);
+    vec3 halfwayDir = normalize(fs_in.LightDir + fs_in.ViewDir);  
     float spec = pow(max(dot(normal, halfwayDir), 0.0), 32.0);
 
     vec3 specular = vec3(0.2) * spec;
--- data/shaders/11_normal_mapping/shader.vs
+++ data/shaders/11_normal_mapping/shader.vs
@@ -8,9 +8,8 @@ layout (location = 4) in vec3 aBitangent;
 out VS_OUT {
     vec3 FragPos;
     vec2 TexCoords;
-    vec3 TangentLightPos;
-    vec3 TangentViewPos;
-    vec3 TangentFragPos;
+	vec3 LightDir;
+    vec3 ViewDir;
 } vs_out;
 
 uniform mat4 projection;
@@ -32,9 +31,13 @@ void main()
     vec3 B = cross(N, T);
     
     mat3 TBN = transpose(mat3(T, B, N));    
-    vs_out.TangentLightPos = TBN * lightPos;
-    vs_out.TangentViewPos  = TBN * viewPos;
-    vs_out.TangentFragPos  = TBN * vs_out.FragPos;
+
+    vec3 TangentFragPos  = TBN * vs_out.FragPos;
+    vec3 TangentLightPos = TBN * lightPos;
+    vec3 TangentViewPos  = TBN * viewPos;
+
+	vs_out.LightDir = normalize(TangentLightPos - TangentFragPos);
+    vs_out.ViewDir = normalize(TangentViewPos - TangentFragPos);
         
     gl_Position = projection * view * model * vec4(aPos, 1.0);
 }
